2413e136b74eaae3c49deb5de5423d96a11ca847
Sage Lichtenwalner <sage@marine.rutgers.edu>
2016-04-13 21:56:50 -0400

    Added parsing of instrument deployments and coefficients

--- datateam/deployments.py
+++ datateam/deployments.py
@@ -6,8 +6,8 @@ import openpyxl as xl
 import re
 import glob
 
-def find(db,reference_designator,deployment_number):
-  """Find an Deployment by reference_designator and deployment_number"""
+def find_deployment(db,reference_designator,deployment_number):
+  """Find a Deployment by reference_designator and deployment_number"""
   sql = 'reference_designator="%s" AND deployment_number="%s"' % (reference_designator, deployment_number)
   result = db.select('deployments',sql,'id')
   if len(result) > 0:
@@ -16,24 +16,60 @@ def find(db,reference_designator,deployment_number):
     return False
 
 
-def save(db,data):
+def save_deployment(db,data):
   """Save an Deployment to the database"""
-  id = find(db,data['reference_designator'],data['deployment_number'])
+  id = find_deployment(db,data['reference_designator'],data['deployment_number'])
   if id == False:
-#     data['created'] = time.strftime('%Y-%m-%d %H:%M:%S')
+    #data['created'] = time.strftime('%Y-%m-%d %H:%M:%S')
     res = db.insert('deployments', data)
     print "Created Deployment: " +data['reference_designator'] +' deployment #' +str(data['deployment_number'])
   else:
-#     data['modified'] = time.strftime('%Y-%m-%d %H:%M:%S')
+    #data['modified'] = time.strftime('%Y-%m-%d %H:%M:%S')
     res = db.update('deployments', id, data)
     print "Updated Deployment: " +data['reference_designator'] +' deployment #' +str(data['deployment_number'])
 
 
+def find_cal(db,reference_designator,deployment_number,cc_name):
+  """Find an Calibration"""
+  sql = 'reference_designator="%s" AND deployment_number="%s" AND cc_name="%s"' % (reference_designator, deployment_number, cc_name)
+  result = db.select('calibrations',sql,'id')
+  if len(result) > 0:
+    return result[0]['id']
+  else:
+    return False
+
+
+def save_cal(db,data):
+  """Save an Calibration to the database"""
+  id = find_cal(db,data['reference_designator'],data['deployment_number'],data['cc_name'])
+  if id == False:
+    #data['created'] = time.strftime('%Y-%m-%d %H:%M:%S')
+    res = db.insert('calibrations', data)
+    print "Created calibration: " +data['reference_designator'] +' deployment #' +str(data['deployment_number']) +' ' +data['cc_name']
+  else:
+    #data['modified'] = time.strftime('%Y-%m-%d %H:%M:%S')
+    res = db.update('calibrations', id, data)
+    print "Updated calibration: " +data['reference_designator'] +' deployment #' +str(data['deployment_number']) +' ' +data['cc_name']
+
+
+def get_deployment_by_barcode(db,mooring_barcode,deployment_number):
+  """Get a Deployment by id"""
+  sql = 'mooring_barcode="%s" AND deployment_number="%s"' % (mooring_barcode,deployment_number)
+  result = db.select('deployments',sql)
+  if len(result) > 0:
+    return result[0]
+  else:
+    return False
+
+
 def load(db):
   """Load Assets into the database"""
+  deployments=[]
   file_mask = "repos/asset-management/deployment/Omaha_Cal_*.xlsx"
   file_list = glob.glob(file_mask)
-  
+
+  # Test File
+  #file_list = ['repos/asset-management/deployment/Omaha_Cal_Info_CE01ISSP_00003.xlsx']
   # Problem files for testing
   #file_list = ['repos/asset-management/deployment/Omaha_Cal_Info_CP05MOAS-GL388_00002.xlsx'] # missing glider
   #file_list = ['repos/asset-management/deployment/Omaha_Cal_Info_CE01ISSP_00002.xlsx']# no inserts
@@ -42,6 +78,7 @@ def load(db):
   #file_list = ['repos/asset-management/deployment/Omaha_Cal_Info_CP05MOAS-GL376_00002.xlsx'] #wrong worksheet name
   #file_list = ['repos/asset-management/deployment/Omaha_Cal_Info_RS03CCAL.xlsx'] #refdes has an extra space
   #file_list = ['repos/asset-management/deployment/Omaha_Cal_Info_RS03INT1.xlsx'] #recover_date has time included
+  #file_list = ['repos/asset-management/deployment/Omaha_Cal_Info_CP01CNSM_00002.xlsx'] #Refdes in asset_cal sheet starts with a #
   
   for xlfile in file_list:
     print "Processing... " +xlfile
@@ -58,10 +95,35 @@ def load(db):
         else:  # Go through the data and upload to the database
           moor_dict = dict(zip(moor_header, row))  # Zip the headers and data rows into a dictionary
           if moor_dict['ref_des'] and moor_dict['deployment_number']:
+            d = moor_dict['ref_des']+'-'+str(moor_dict['deployment_number'])
             moor_dict = clean_mooring_data(moor_dict)  # Create valid data types for mysql db
             #print moor_dict
-            save(db,moor_dict)
+            save_deployment(db,moor_dict)
+            deployments.append(d)
+
+    if set(['Asset_Cal_Info']).issubset(wb.get_sheet_names()):
+      m_data = crawl_worksheet(wb['Asset_Cal_Info'])
+      cal_header = [str(x).lower().replace(" ", "_") for x in m_data[0]]
   
+      # Iterate through 'Asset_Cal_Info' sheet data matrix
+      for row in m_data[1:]:  # Skip the first row, because all cal sheets have headers
+        if row == [None] * len(row):  # Check for empty rows and skip
+          continue
+        else:  # Go through the data and upload to the database
+          cal_dict = dict(zip(cal_header, row))  # Zip the headers and data rows into a dictionary
+    
+          if cal_dict['ref_des'] and cal_dict['deployment_number'] and cal_dict['ref_des'][0] != '#':
+            d = cal_dict['ref_des']+'-'+str(cal_dict['deployment_number'])
+            if d not in deployments:
+              mooring = get_deployment_by_barcode(db,cal_dict['mooring_ooibarcode'],cal_dict['deployment_number']) # Get parent moooring
+              if mooring:
+                mooring['reference_designator'] = cal_dict['ref_des'] # Override values
+                mooring.pop('id')
+                save_deployment(db,mooring) # Save deployment
+              deployments.append(d)
+            if cal_dict['calibration_cofficient_name']:
+              save_cal(db,clean_cal_data(cal_dict)) # Save calibration info
+              
 
 def conversion(old):
     """
@@ -99,7 +161,7 @@ def crawl_worksheet(ws):
 
 
 def clean_mooring_data(data_dict):
-    """Clean deployment table items and rename some column"""
+    """Clean deployment table items and rename some columns"""
     
     data_dict['reference_designator'] = data_dict['ref_des'].strip() # Remove extra spaces
     data_dict['mooring_barcode'] = data_dict['mooring_ooibarcode']
@@ -163,6 +225,22 @@ def clean_mooring_data(data_dict):
         data_dict['water_depth'] = re.sub("[^0123456789]", "", str(data_dict['water_depth']))
         if data_dict['water_depth'].strip() == '':
           data_dict.pop('water_depth')
+
+    return data_dict
+
+
+def clean_cal_data(data_dict):
+    """Clean asset_cal table items and rename some columns"""
+    
+    data_dict['reference_designator'] = data_dict['ref_des'].strip() # Remove extra spaces
+    data_dict['mooring_barcode'] = data_dict['mooring_ooibarcode']
+    data_dict['sensor_barcode'] = data_dict['sensor_ooibarcode']
+    data_dict['cc_name'] = data_dict['calibration_cofficient_name']
+    data_dict['cc_value'] = data_dict['calibration_cofficient_value']
+
+    m_h = ['reference_designator', 'mooring_barcode', 'mooring_serial_number', 'deployment_number', 
+           'sensor_barcode', 'sensor_serial_number', 'cc_name', 'cc_value']
+    data_dict = remove_extraneous_columns(m_h, data_dict)
 
     return data_dict
 
