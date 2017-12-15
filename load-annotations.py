0db6ef84b63e53625f6e5ee5c2e1c67c59a05342
Sage Lichtenwalner <sage@marine.rutgers.edu>
2017-12-15 15:11:34 -0500

    Change annotation import to pull from OOI Net

--- load-annotations.py
+++ load-annotations.py
@@ -3,10 +3,8 @@
 
 import datateam
 import argparse
-import glob
-import csv
-import os
-import dateutil.parser
+import datetime
+import requests
 
 # Command Line Arguments
 parser = argparse.ArgumentParser(description='OOI Data Team Portal - Annotation Importer')
@@ -17,10 +15,20 @@ parser.add_argument('-s','--server',
 args = parser.parse_args()
 
 
+def load_m2m_config():
+  """Load M2M configuration"""
+  import ConfigParser
+  config = ConfigParser.ConfigParser()
+  config.readfp(open('config.cfg'))
+  return {
+    'username': config.get('ooinet','username'),
+    'password': config.get('ooinet','password')
+  }   
+
 # Allowed columns
-columns = ['reference_designator', 'deployment','method','stream','parameter',
-  'start_datetime','end_datetime','annotation','status',
-  'redmine_issue','todo','reviewed_by','reviewed_date']
+columns = ['id','reference_designator','method','stream','parameter',
+  'start_datetime','end_datetime','annotation',
+  'exclusionFlag','qcFlag','source']
 
 def save(db, data):
   """Save annotation to the database"""
@@ -28,54 +36,36 @@ def save(db, data):
   res = db.insert('annotations', data)
   print "Added: %s %s" % (data['reference_designator'], (data['start_datetime'] if 'start_datetime' in data else ''))
 
-#   id = find(db,data['reference_designator'],data['month'])
-#   if id == False:
-#     #data['created'] = time.strftime('%Y-%m-%d %H:%M:%S')
-#     res = db.insert('monthly_stats', data)
-#     print "Created: " +data['reference_designator'] +' ' +data['month']
-#   else:
-#     #data['modified'] = time.strftime('%Y-%m-%d %H:%M:%S')
-#     res = db.update('monthly_stats', id, data)
-#     #print "Updated: " +data['reference_designator'] +' ' +data['month']
+
+def create_url(start_id):
+  url = ('https://ooinet.oceanobservatories.org/api/m2m/12580/anno?max_100&start_id={start_id}').format(start_id=start_id)
+  return url
 
 
-def convert_deployment(s):
-  if s.startswith('D'):
-    s = s[1:]
-    return int(s)
-  elif s.isdigit(): 
-    return int(s)
-  else:
-    return False
-
-def fix_time(s):
-  if s:
-    v = dateutil.parser.parse(s)
-    return v.strftime('%Y-%m-%d %H:%M:%S')
-  else:
-    return None
-# Old version
-#   if s.endswith('Z'):
-#     return s[:-1]
-#   else:
-#     return s
+def convert_time(s):
+  s = s/1000; #Convert from milliseconds 
+  return datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d %H:%M:%S')
 
 
-def process_row(row,reference_designator,method='',stream='',parameter=''):
-  row['reference_designator'] = reference_designator.strip()
-  row['method'] = method
-  row['stream'] = stream
-  row['parameter'] = parameter
-  row['deployment'] = convert_deployment(row['Deployment'])
-  row['start_datetime'] = fix_time(row['StartTime'])
-  row['end_datetime'] = fix_time(row['EndTime'])
-  row['annotation'] = row['Annotation'].decode('utf-8', errors='replace')
-  row['status'] = row['Status']
-  row['redmine_issue'] = row['Redmine#']
-  row['todo'] = row['Todo']
-  row['reviewed_by'] = row['ReviewedBy'] if 'ReviewedBy' in row else ''
-  row['reviewed_date'] = fix_time(row['ReviewedDate']) if 'ReviewedDate' in row else ''
-  #  print '%s - %s' % (row['reviewed_date'],fix_time(row['reviewed_date']))
+def process_row(row):
+  #row['id'] = row['id']
+  row['reference_designator'] = row['subsite'] 
+  if (row['node']):
+    row['reference_designator'] = "%s-%s" % (row['subsite'], row['node'])
+  if (row['sensor']):
+    row['reference_designator'] = "%s-%s-%s" % (row['subsite'], row['node'], row['sensor'])
+  #row['method'] = row['method']
+  #row['stream'] = row['stream']
+  #row['parameter'] = row['parameter']
+  if (row['beginDT']):
+    row['start_datetime'] = convert_time(row['beginDT'])
+  if (row['endDT']):
+    row['end_datetime'] = convert_time(row['endDT'])
+  #row['annotation'] = row['annotation'].decode('utf-8', errors='replace')
+  #row['exclusionFlag'] = 1 if row['exclusionFlag']=='true' else 0
+  #row['qcFlag'] = row['qcFlag']
+  #row['source'] = row['source'] #if 'ReviewedBy' in row else ''
+  return row
 
 
 def load_annotations(db):
@@ -83,37 +73,22 @@ def load_annotations(db):
   r = db.truncate_table('annotations')
   print "Truncated annotations table"
 
-  file_mask = "repos/annotations/annotations/*"
-  site_list = glob.glob(file_mask)
-  for site_directory in site_list:
-    # First, process the Site Annotation file
-    site_csv_list = glob.glob(site_directory + '/*.csv')
-    for ifile in site_csv_list:
-      print "Loading: " + ifile
-      with open(ifile, 'rb') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-          process_row(row,row['Level'])
-          save(db,row)
-    # Second, process the Instrument Directories, and the Stream files in them
-    subdir_list = glob.glob(site_directory+'/*/')
-    for subdirectory in subdir_list:
-      stream_csv_list = glob.glob(subdirectory + '/*.csv')
-      for ifile in stream_csv_list:
-        print "Loading: " + ifile
-        rd = os.path.dirname(ifile).split('/')[-1]
-        method = os.path.basename(ifile).split('-')[0]
-        stream = os.path.basename(ifile).split('-')[1].split('.')[0].replace('_parameters','')
-        with open(ifile, 'rb') as csvfile:
-          reader = csv.DictReader(csvfile)
-          for row in reader:
-            if 'Status' in row and row['Status']:
-              if os.path.basename(ifile).find('parameters')>0:
-                process_row(row,rd,method,stream,row['Level'])
-                save(db,row)
-              else:
-                process_row(row,rd,method,stream)
-                save(db,row)
+  # Load config information
+  config = load_m2m_config()
+
+  # Extract All Annoations
+  result_length = 100
+  start_id = 1
+  while result_length ==100:
+    url = create_url(start_id)
+    # Grab Annotations data from API
+    data = requests.get(url, auth=(config['username'],config['password'])).json()
+    for row in data:
+      row = process_row(row)
+      save(db,row) # Save to database
+    result_length = len(data)
+    start_id = data[-1]['id'] + 1
+    print 'Finished: %d Annotations, Next id is %d' % (result_length,start_id)
 
 
 def main():
