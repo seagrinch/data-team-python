46cc86d3a0b334f72b57bc4f04b56c27babb9564
Sage Lichtenwalner <sage@marine.rutgers.edu>
2018-04-11 12:29:24 -0400

    Change path for daily stats files

--- load-dailystats.py
+++ load-dailystats.py
@@ -4,15 +4,6 @@
 import datateam
 import argparse
 
-# Command Line Arguments
-parser = argparse.ArgumentParser(description='OOI Data Team Portal - Daily Stats Data Importer')
-parser.add_argument('-s','--server',
-  choices=['production','development'],
-  default='development',
-  help='Database server')
-args = parser.parse_args()
-
-
 def main():
   """Main function for command line execution"""
   db = datateam.MysqlPython()
@@ -27,7 +18,7 @@ def main():
   
   # Loop over each file and import
   for ff in arys:
-    dfile = 'daily_stats/%s_stream_final.csv' % (ff)
+    dfile = '%s/%s/stream/%s_stream_final.csv' % (args.directory,ff,ff)
     sql = "LOAD DATA LOCAL INFILE '%s' INTO TABLE stream_stats FIELDS TERMINATED BY ',' IGNORE 1 LINES (reference_designator, method, stream, date, status);" % dfile
     a = db.sqlquery(sql);
     print "%s rows inserted from %s " % ( a.rowcount, dfile )
@@ -38,7 +29,7 @@ def main():
 
   # Loop over each file and import
   for ff in arys:
-    dfile = 'daily_stats/%s_refdes_final.csv' % (ff)
+    dfile = '%s/%s/refdes/%s_refdes_final.csv' % (args.directory,ff,ff)
     sql = "LOAD DATA LOCAL INFILE '%s' INTO TABLE instrument_stats FIELDS TERMINATED BY ',' IGNORE 1 LINES (reference_designator, date, status);" % dfile
     a = db.sqlquery(sql);
     print "%s rows inserted from %s " % ( a.rowcount, dfile )
@@ -51,4 +42,14 @@ def main():
 
 # Run main function when in comand line mode        
 if __name__ == '__main__':
+  # Command Line Arguments
+  parser = argparse.ArgumentParser(description='OOI Data Team Portal - Daily Stats Data Importer')
+  parser.add_argument('-s','--server',
+    choices=['production','development'],
+    default='development',
+    help='Database server')
+  parser.add_argument('-d','--directory',
+    default='daily_stats',
+    help='Stats data directory')
+  args = parser.parse_args()
   main()
