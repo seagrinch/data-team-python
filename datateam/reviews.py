c718257636504b7fc9bdabfc6a26c5055fa5fa4e
Sage Lichtenwalner <sage@marine.rutgers.edu>
2018-11-30 16:37:56 -0500

    Tweaks to reviews import
    Added created timestamp
    Update review status when importing test results, if not already specified

--- datateam/reviews.py
+++ datateam/reviews.py
@@ -5,6 +5,7 @@ import fnmatch
 import os
 import pandas as pd
 import datetime
+import time
 
 
 def find_review(db,reference_designator,deployment,stream):
@@ -21,12 +22,23 @@ def save_review(db,data):
   """Save a Review to the database"""
   id = find_review(db,data['reference_designator'],data['deployment'],data['stream'])
   if id == False:
+    data['created'] = time.strftime('%Y-%m-%d %H:%M:%S')
     res = db.insert('reviews', data)
     print "Created Review: " + data['reference_designator'] + ' ' + str(data['deployment']) + ' ' + str(data['stream'])
   else:
+    #data['modified'] = time.strftime('%Y-%m-%d %H:%M:%S')
     res = db.update('reviews', id, data)
     print "Updated Review: " + data['reference_designator'] + ' ' + str(data['deployment']) + ' ' + str(data['stream'])
 
+
+def get_review(db,reference_designator,deployment,stream):
+  """Get a Review record"""
+  sql = 'reference_designator="%s" AND deployment="%s" AND stream="%s"' % (reference_designator, deployment, stream)
+  result = db.select('reviews',sql)
+  if len(result) > 0:
+    return result[0]
+  else:
+    return False
 
 def load(db):
   """Load Reviews into the database"""
@@ -36,7 +48,7 @@ def load(db):
   'start_days_missing','end_days_missing','location_diff_km',
   'n_days_deployed','n_timestamps','n_days','deploy_depth','pressure_mean','pressure_diff','sampling_rate_seconds',
   'gaps_num','gaps_num_days','timestamp_test','n_science_vars','valid_data_test',
-  'variable_comparison_test','full_dataset_test','coordinate_test','file_downloaded']
+  'variable_comparison_test','full_dataset_test','coordinate_test','file_downloaded','status']
 
   # Find summary files
   file_mask = "repos/data-review-tools/data_review/output/"
@@ -53,6 +65,9 @@ def load(db):
       for row in reader:
         row['reference_designator'] = ifile.split('/')[-1][0:27]
         row['deployment'] = int(row['deployment'][-3:])
+        review = get_review(db,row['reference_designator'],row['deployment'],row['stream'])
+        if (review['status'] == None):
+          row['status'] = 'Tested'
         data = remove_extraneous_columns(columns, row)
         save_review(db,data)
 
@@ -67,18 +82,20 @@ def find_deployments(db,reference_designator):
     return False
 
 
-def yes_no(answer):
+def yes_no(question):
+    """Prompt user with a yes/no question"""
     yes = set(['yes'])
     no = set(['no','n'])
     
     while True:
-        choice = raw_input(answer).lower()
+        choice = raw_input(question).lower()
         if choice in yes:
            return True
         elif choice in no:
            return False
         else:
            print "Please respond with 'yes' or 'no'\n"
+
 
 def load_baseline(db):
   """Load Review Baseline into the database"""
@@ -88,10 +105,10 @@ def load_baseline(db):
   for index, row in baseline.iterrows():
     row = row.to_dict()
     row['preferred_method'] = row['method']
-#     print(row['reference_designator'])
+    # print(row['reference_designator'])
     deployments = find_deployments(db,row['reference_designator'])
     for d in deployments:
-#       print(str(d['deployment_number']) + str(d['stop_date']))
+      # print(str(d['deployment_number']) + str(d['stop_date']))
       if(d['stop_date'] is not None and d['stop_date'] < datetime.datetime(2018, 10, 1)):
         row['deployment'] = d['deployment_number']
         data = remove_extraneous_columns(columns, row)
